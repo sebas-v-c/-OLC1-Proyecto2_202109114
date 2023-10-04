08db2eb1c4815b2eb0a6b412579cb5f88e4dd21d
Sebastian Vasquez <sebas.vasquez010@gmail.com>
2023-10-04 17:14:35 -0600

    simple changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -20,7 +20,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 2)}>
+        <button onClick={() => setCount((count) => count + 3)}>
           count is {count}
         </button>
         <p>
