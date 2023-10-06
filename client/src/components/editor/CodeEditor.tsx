3d26ee4f67f8d3f900e04814bb8a90871dcd2ad6
Sebastian Vasquez <sebas.vasquez010@gmail.com>
2023-10-06 10:12:12 -0600

    code editor finished

--- client/src/components/editor/CodeEditor.tsx
+++ client/src/components/editor/CodeEditor.tsx
@@ -1,7 +1,30 @@
 import { useState } from "react";
-import Editor from "@monaco-editor/react";
+import { Editor } from "@monaco-editor/react";
 
-export default function CodeEditor({ onChange, language, code, theme }) {
+type Props = {
+    onChange: (code: string, val: string) => void;
+    code: string;
+    theme: string;
+};
+
+export default function CodeEditor({ onChange, code, theme}: Props) {
     const [value, setValue] = useState(code || "");
+
+    function handleEditorChange(value: any){
+        setValue(value);
+        onChange("code", value);
+    }
+
+    return (
+        <Editor
+            height={"85vh"}
+            width={`100%`}
+            language={"mysql"}
+            value={value}
+            theme={theme}
+            defaultValue="// Some code here"
+            onChange={handleEditorChange}
+        />
+    );
 
 }
--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -6,4 +6,5 @@ export default function MainPage(){
         <>
             <CodeEditor/>
         </>
+    );
 }
