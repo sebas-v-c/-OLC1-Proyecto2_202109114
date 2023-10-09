96aaf9b3a4d98acb8cdf6a7c7b6f8dd7fe2cf0c8
Sebastian Vasquez <sebas.vasquez010@gmail.com>
2023-10-09 16:07:35 -0600

    Errors output finished

--- client/src/components/editor/output/OutputErrors.tsx
+++ client/src/components/editor/output/OutputErrors.tsx
@@ -1,8 +1,49 @@
-import React from 'react';
+import "../../../scss/Errors.scss"
 
-export default function OutputErrors(){
+interface Table {
+    lex: string;
+    line: number;
+    column: number;
+}
+
+interface Token extends Table {
+    token: string;
+}
+
+interface LexError extends Table {
+    desc: string;
+}
+
+interface SynError extends LexError {
+}
+
+interface Symbol {
+    var: string;
+    type: string;
+    value: any;
+}
+
+
+type ResObj = {
+    tokens: Token[];
+    lexErr: LexError[];
+    synErr: SynError[];
+    symTable: Symbol[];
+}
+
+type Props = {
+    content: string;
+}
+
+export default function OutputErrors({ content }: Props){
 
     return(
-        <div></div>
+        <div className="terminal">
+            <h1>Tokens</h1>
+            <h1>Errors</h1>
+            <h2>Lexical Errors</h2>
+            <h2>Syntax Errors</h2>
+            <h1>Symbols Table</h1>
+        </div>
     );
 }
