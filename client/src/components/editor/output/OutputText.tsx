118b8fe808fcd596b71ea3d66e3a55c261ce4d43
Sebastian Vasquez <sebas.vasquez010@gmail.com>
2023-10-09 15:42:50 -0600

    output text terminal finished

--- client/src/components/editor/output/OutputText.tsx
+++ client/src/components/editor/output/OutputText.tsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import { useEffect, useRef } from "react";
+import "../../../scss/Terminal.scss"
 
-export default function OutputText(){
+type Props = {
+    content: string;
+}
+
+
+export default function OutputText({ content }: Props){
+    const terminalRef = useRef(null);
+
+    useEffect(() => {
+        if (terminalRef.current) {
+            terminalRef.current.scrollTop = terminalRef.current.scrollHeight;
+        }
+    })
+
+    const lines: string[] = content.split('\n');
 
     return(
-        <div></div>
+        <div className="terminal" ref={terminalRef}>
+            {lines.map((line, index) => (
+                <div key={index}>{line}</div>
+            ))}
+        </div>
     );
 }
